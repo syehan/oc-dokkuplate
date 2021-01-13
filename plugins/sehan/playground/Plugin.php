000c2f7fe98d0fbfe633926c8cefd875fa982226
syehan <muhammad.syehan@gmail.com>
2021-01-13 11:27:00 +0700

    add dokku scale cron, create plugin playground

---
+++ plugins/sehan/playground/Plugin.php
@@ -0,0 +1,103 @@
+<?php namespace Sehan\Playground;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Playground Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Playground',
+            'description' => 'No description provided yet...',
+            'author'      => 'Sehan',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'Sehan\Playground\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'sehan.playground.some_permission' => [
+                'tab' => 'Playground',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'playground' => [
+                'label'       => 'Playground',
+                'url'         => Backend::url('sehan/playground/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['sehan.playground.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+
+    public function registerSchedule($schedule)
+    {
+        $schedule->call(function () {
+            TraceLog('dokku schedule trigger ' . now()->format(' j-f-Y H:i:s'));
+        })->everyMinute();
+    }
+}
